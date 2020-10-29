3eace9409c240f30b4a875961cfae7b2b4292299
ValiantWolf <25witt25@gmail.com>
2020-10-29 16:07:38 -0600

    windchill

--- lesson6/js/windChill.js
+++ lesson6/js/windChill.js
@@ -1,3 +1,4 @@
+// calculates windchill
     let t= parseFloat(document.getElementById('t').textContent);
     let s= parseFloat(document.getElementById('s').textContent);
     let compute= 35.74+0.6215*t-35.75*Math.pow(s,0.16)+0.4275*t*Math.pow(s,0.16);
