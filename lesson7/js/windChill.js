7d4d6d439da5ca72f9184a5eb2a69609ce8aed9d
ValiantWolf <25witt25@gmail.com>
2020-10-30 15:21:41 -0600

    commit

--- lesson7/js/windChill.js
+++
@@ -1,9 +0,0 @@
-    let t= parseFloat(document.getElementById('t').textContent);
-    let s= parseFloat(document.getElementById('s').textContent);
-    let compute= 35.74+0.6215*t-35.75*Math.pow(s,0.16)+0.4275*t*Math.pow(s,0.16);
-    if(t <= 50 && s >= 3){
-        document.querySelector(".w").textContent= Math.ceil(compute)+ "°";
-    }
-    else{
-        document.querySelector(".w").textContent=  "N/A";
-    }
