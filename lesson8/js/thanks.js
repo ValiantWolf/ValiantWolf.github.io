a438df8ddef0af9d4b794480387c85a58832b0b1
ValiantWolf <25witt25@gmail.com>
2020-11-07 19:42:07 -0700

    send

--- lesson8/js/thanks.js
+++ lesson8/js/thanks.js
@@ -1,3 +1,3 @@
 function thanks() {
-    window.location.href="https://valiantwolf.github.io/ValiantWolf.github.io/lesson8/stormcenter.html";
+    window.location.href="https://valiantwolf.github.io/ValiantWolf.github.io/lesson8/thanks.html";
 }
