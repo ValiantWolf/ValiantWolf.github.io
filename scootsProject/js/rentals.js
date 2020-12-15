548f8e69eb41f835605cfa328c9e860efdc54c8f
ValiantWolf <25witt25@gmail.com>
2020-12-15 12:42:17 -0700

    commit

--- scootsProject/js/rentals.js
+++ scootsProject/js/rentals.js
@@ -1,12 +1,48 @@
-const requestURL = 'https://valiantwolf.github.io/ValiantWolf.github.io/scootsProject/data/rental.JSON';
-
-fetch(requestURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonObject) {
-    console.log(jsonObject);
-    const model = jsonObject['model']
-    console.log(model);
+document.addEventListener('DOMContentLoaded', ( )=> {
+    document.getElementById('rentalType').addEventListener('input', handleSelect);
+})
+function handleSelect(ev) {
+    var rentalType = parseFloat(document.getElementById('rentalType').value);
+    switch(rentalType) {
+        case 1:
+            rentalType = 1;
+            console.log(1);
+            break;
+        case 2:
+            rentalType = 2;
+            console.log(2);
+            break;
+        case 3:
+            rentalType = 3;
+            console.log(3);
+            break;
+        case 4:
+            rentalType = 4;
+            console.log(4);
+            break;
+        case 5:
+            rentalType = 5;
+            console.log(5);
+            break;
+        case 6:
+            rentalType = 6;
+            console.log(6);
+            break;
+        default:
+            rentalType = 1;
+            console.log(1);
+            break;
+    }
+    const data = "https://valiantwolf.github.io/ValiantWolf.github.io/scootsProject/data/rental.JSON"
+    fetch(data)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        document.getElementById('persons').innerHTML = data.rental[rentalType - 1].info.maxPersons;
+        document.getElementById('price1').innerHTML = "$" + data.rental[rentalType - 1].info.reservation.halfDay;
+        document.getElementById('price2').innerHTML = "$" + data.rental[rentalType - 1].info.reservation.fullDay;
+        document.getElementById('price3').innerHTML = "$" + data.rental[rentalType - 1].info.walkIn.halfDay;
+        document.getElementById('price4').innerHTML = "$" + data.rental[rentalType - 1].info.walkIn.fullDay;
+        document.getElementById('vehicleRentals').setAttribute('src', 'assets/vehicle' + rentalType + '.jpg' );
     });
-    
+}
